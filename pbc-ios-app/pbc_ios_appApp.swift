ad3f15cf5deb97d8f4971a9eaf3e06be581898c0
Kavimal Wijewardana <kavimalw@gmail.com>
2025-12-08 16:45:06 -0500

    Initial Commit

---
+++ pbc-ios-app/pbc_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pbc_ios_appApp.swift
+//  pbc-ios-app
+//
+//  Created by Kavimal Wijewardana on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct pbc_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
